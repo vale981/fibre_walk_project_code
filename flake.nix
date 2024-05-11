00500adeeaadc6ea8fc100c228946a08b7598f30
Valentin Boettcher <hiro@protagon.space>
2024-05-11 11:52:04 -0400

    switch to a flake

--- flake.nix
+++ flake.nix
@@ -1,34 +1,103 @@
 {
+  description = "Analysis code for the realization of the NHQW in fibre loops.";
+
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  };
-
-  outputs = {nixpkgs, ...}: let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in {
-    devShells.${system}.default = pkgs.mkShell {
-      packages = [
-        pkgs.python3
-        pkgs.poetry
-      ];
-
-      env = {
-        # Workaround in linux: python downloads ELF's that can't find glibc
-        # You would see errors like: error while loading shared libraries: name.so: cannot open shared object file: No such file or directory
-        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-          pkgs.stdenv.cc.cc
-          # Add any missing library needed
-          # You can use the nix-index package to locate them, e.g. nix-locate -w --top-level --at-root /lib/libudev.so.1
-        ];
-
-        # Put the venv on the repo, so direnv can access it
-        POETRY_VIRTUALENVS_IN_PROJECT = "true";
-        POETRY_VIRTUALENVS_PATH = "{project-dir}/.venv";
-
-        # Use python from path, so you can use a different version to the one bundled with poetry
-        POETRY_VIRTUALENVS_PREFER_ACTIVE_PYTHON = "true";
-      };
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+
+  outputs = inputs @ { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        poetry2nix = inputs.poetry2nix.lib.mkPoetry2Nix { inherit pkgs;};
+      in
+        {
+          packages = {
+            fibreMagic = poetry2nix.mkPoetryApplication {
+              projectDir = self;
+              preferWheels = true;
+
+
+              overrides = poetry2nix.overrides.withDefaults (final: prev: {
+                matplotlib = with pkgs; prev.matplotlib.overridePythonAttrs (
+                  old:
+                  let
+                    interactive = true;
+
+                    passthru = {
+                      config = {
+                        directories = { basedirlist = "."; };
+                        libs = {
+                          system_freetype = true;
+                          system_qhull = true;
+                        };
+                      };
+                    };
+
+                    inherit (pkgs) tk tcl wayland qhull;
+                    inherit (pkgs.xorg) libX11;
+                  in
+                    {
+                      XDG_RUNTIME_DIR = "/tmp";
+
+                      buildInputs = old.buildInputs or [ ] ++ [
+                        pkgs.which
+                      ];
+
+                      propagatedBuildInputs = (old.propagatedBuildInputs or [ ]) ++ [
+                        final.certifi
+                        pkgs.libpng
+                        pkgs.freetype
+                        qhull
+                      ]
+                      ++ [ pkgs.cairo pkgs.librsvg final.pycairo pkgs.gtk3 pkgs.gtk4 pkgs.gobject-introspection final.pygobject3 ]  ;
+
+                      nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [
+                        pkg-config
+                      ] ++ [
+                        final.setuptools-scm
+                      ];
+
+                      # Clang doesn't understand -fno-strict-overflow, and matplotlib builds with -Werror
+                      hardeningDisable = if stdenv.isDarwin then [ "strictoverflow" ] else [ ];
+
+                      passthru = old.passthru or { } // passthru;
+
+                      MPLSETUPCFG = pkgs.writeText "mplsetup.cfg" (lib.generators.toINI { } passthru.config);
+
+                    }
+                );
+
+
+              });
+            };
+            default = self.packages.${system}.fibreMagic;
+          };
+
+          # Shell for app dependencies.
+          #
+          #     nix develop
+          #
+          # Use this shell for developing your app.
+          devShells.default = pkgs.mkShell {
+            inputsFrom = [ self.packages.${system}.fibreMagic ];
+            package = with pkgs; [
+              ruff pyright
+            ];
+          };
+
+          # Shell for poetry.
+          #
+          #     nix develop .#poetry
+          #
+          # Use this shell for changes to pyproject.toml and poetry.lock.
+          devShells.poetry = pkgs.mkShell {
+            packages = [ pkgs.poetry ];
+          };
+        });
 }
